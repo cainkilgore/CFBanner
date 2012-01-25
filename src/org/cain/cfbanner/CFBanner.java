9cb18025a7a372c7e92838049b8c4f577c3dab73
Cain <cain.donaghey@live.co.uk>
2012-01-25 00:58:01 +0000

    Small Code Optimisation.

--- src/org/cain/cfbanner/CFBanner.java
+++ src/org/cain/cfbanner/CFBanner.java
@@ -8,16 +8,12 @@ import org.cain.cfbanner.listeners.playerListener;
 public class CFBanner extends JavaPlugin {
 	
 	public void onEnable() {
-		registerEvents();
+		getServer().getPluginManager().registerEvent(Type.PLAYER_LOGIN, new playerListener(), Priority.Normal, this);
 		System.out.println("[CFBanner] CFBanner has been enabled!");
 	}
 	
 	public void onDisable() {
 		System.out.println("[CFBanner] CFBanner has been disabled!");
-	}
-	
-	public void registerEvents() {
-		getServer().getPluginManager().registerEvent(Type.PLAYER_LOGIN, new playerListener(), Priority.Normal, this);
 	}
 
 }
