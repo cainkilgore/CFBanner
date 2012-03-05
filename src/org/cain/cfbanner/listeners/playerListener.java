41a80696c79f1d434cfbccb8e524e2fa2165191d
jascotty2 <jascottytechie@gmail.com>
2012-03-05 11:53:30 -0600

    update for new bukkit compatibility

--- src/org/cain/cfbanner/listeners/playerListener.java
+++ src/org/cain/cfbanner/listeners/playerListener.java
@@ -1,15 +1,17 @@
 package org.cain.cfbanner.listeners;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
-public class playerListener extends PlayerListener {
+public class playerListener implements Listener {
 	
 	String ZOMBE_FLY = "§f §f §1 §0 §2 §4";
 	String ZOMBE_CHEAT = "§f §f §2 §0 §4 §8";
 	String ZOMBE_NOCLIP = "§f §f §4 §0 §9 §6";
 	
+	@EventHandler
 	public void onPlayerLogin(PlayerLoginEvent e) {
 		Player p = e.getPlayer();
 		if(!p.hasPermission("cfbanner.zombe.fly")) {
@@ -21,7 +23,6 @@ public class playerListener extends PlayerListener {
 		if(!p.hasPermission("cfbanner.zombe.noclip")) {
 			p.sendMessage(ZOMBE_NOCLIP);
 		}
-		return;
 	}
 
 }
